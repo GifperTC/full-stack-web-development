f206f9e8bda573bef760494bd298bbe15de1750f
Nutta Sittipongpanich <giftbbt@gmail.com>
2022-02-26 07:06:06 +0000

    Add GET and POST

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,28 @@
-<script>
+<script context="module" lang="ts">
+  import type { Load } from "@sveltejs/kit";
+
+  export const load: Load = async ({ fetch }) => {
+    const res = await fetch("/todos.json");
+
+    if (res.ok) {
+      const todos = await res.json();
+      return {
+        props: { todos }
+      }
+    }
+
+    const { message } = await res.json();
+    return {
+      error: new Error(message)
+    }
+  }
+
+</script>
+
+<script lang="ts">
     import TodoItem from "$lib/todo-item.svelte";
+
+    export let todos: Todo[];
 
     const title = "Todo";
 </script>
@@ -20,9 +43,9 @@
     width: 100%;
     padding: 0.5em 1em 0.3em 1em;
     box-sizing: border-box;
-	background: rgba(255, 255, 255, 0.05);
-	border-radius: 8px;
-	text-align: center;
+	  background: rgba(255, 255, 255, 0.05);
+	  border-radius: 8px;
+	  text-align: center;
   }
 
   .todos :global(input) {
@@ -44,11 +67,11 @@
 <div class="todos">
     <h1>{title}</h1>
 
-    <form action="" method="" class="new">
+    <form action="/todos.json" method="post" class="new">
         <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo" />
     </form>
 
-    <TodoItem />
-    <TodoItem />
-    <TodoItem />
+    {#each todos as todo}
+    <TodoItem {todo}/>
+  {/each}
 </div>
