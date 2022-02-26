6bfdb3cff3de83ee0c3ecc4dad24ed07c2b9ec89
Nutta Sittipongpanich <giftbbt@gmail.com>
2022-02-26 11:43:15 +0000

    use @prisma/client.

--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -1,40 +1,54 @@
 import type { Request } from "@sveltejs/kit";
+import PrismaClient from "$lib/prisma";
 
-let todos: Todo[] = [];
+const prisma = new PrismaClient();
 
-export const api = (request: Request, data?: Record<string, unknown>) => {
-    let body = {};
-    let status = 500;
+export const api = async (request: Request, data?: Record<string, unknown>) => {
+  let body = {};
+  let status = 500;
 
-    switch (request.method.toUpperCase()) {
-        case "GET":
-          body = todos;
-          status = 200;
-          break;
-        case "POST":
-          todos.push(data as Todo);
-          body = data as Todo;
-          status = 201;
-        case "DELETE":
-          todos = todos.filter(todo => todo.uid !== request.params.uid)
-          status = 200;
-          break;
-        case "PATCH":
-          todos = todos.map(todo => {
-            if (todo.uid === request.params.uid) {
-              if (data.text) todo.text = data.text as string;
-              else todo.done = data.done as boolean;
-            }
-            return todo;
-          });
-          status = 200;
-          body = todos.find(todo => todo.uid === request.params.uid);
-          break;
-        default:
-            break;
-    }
+  switch (request.method.toUpperCase()) {
+    case "GET":
+      body = await prisma.todo.findMany();
+      status = 200;
+      break;
+    case "POST":
+      body = await prisma.todo.create({
+        data: {
+          created_at: data.created_at as Date,
+          done: data.done as boolean,
+          text: data.text as string
+        }
+      })
+      status = 201;
+      break;
+    case "DELETE":
+      body = await prisma.todo.delete({
+        where: {
+          uid: request.params.uid
+        }
+      })
+      // todos = todos.filter(todo => todo.uid !== request.params.uid)
+      status = 200;
+      break;
+    case "PATCH":
+      body = await prisma.todo.update({
+        where: {
+          uid: request.params.uid
+        },
+        data: {
+          done: data.done,
+          text: data.text
+        }
+      })
+      status = 200;
+      break;
+  
+    default:
+      break;
+  }
 
-    if (request.method.toUpperCase() !== "GET" &&
+  if (request.method.toUpperCase() !== "GET" &&
     request.headers.accept !== "application/json") {
     return {
       status: 303,
@@ -44,8 +58,8 @@ export const api = (request: Request, data?: Record<string, unknown>) => {
     };
   }
 
-    return {
-        status,
-        body
-    }
+  return {
+    status,
+    body
+  }
 }
