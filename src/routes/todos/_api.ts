fa886c0aab55ca686a5b76a8ff35030332a80cb6
Nutta Sittipongpanich <giftbbt@gmail.com>
2022-02-26 10:41:27 +0000

    Add PATCH endpoint - update text

--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -2,27 +2,34 @@ import type { Request } from "@sveltejs/kit";
 
 let todos: Todo[] = [];
 
-export const api = (request: Request, todo?: Todo) => {
+export const api = (request: Request, data?: Record<string, unknown>) => {
     let body = {};
     let status = 500;
 
     switch (request.method.toUpperCase()) {
         case "GET":
-            body = todos;
-            status = 200;
-            break;
+          body = todos;
+          status = 200;
+          break;
         case "POST":
-            todos.push(todo);
-            body = todos;
-            status = 201;
+          todos.push(data as Todo);
+          body = data as Todo;
+          status = 201;
         case "DELETE":
-            todos = todos.filter(todo => todo.uid !== request.params.uid)
-            status = 200;
-            break;
-    
+          todos = todos.filter(todo => todo.uid !== request.params.uid)
+          status = 200;
+          break;
+        case "PATCH":
+          todos = todos.map(todo => {
+            if (todo.uid === request.params.uid) {
+              todo.text = data.text as string;
+            }
+            return todo;
+          });
+          status = 200;
+          break;
         default:
             break;
-
     }
 
     if (request.method.toUpperCase() !== "GET"){
